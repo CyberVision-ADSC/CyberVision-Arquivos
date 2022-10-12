70039ab5e2fa9958e0f0e09750dcffcc7b12a393
Bruno Cara <bruno.cara@sptech.school>
2022-10-12 19:38:14 -0300

    atualizando script

--- Banco de Dados/CyberVision script.sql
+++ Banco de Dados/CyberVision script.sql
@@ -14,7 +14,8 @@ CREATE TABLE faculdade(
   id_usuario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(40),
   email VARCHAR(40),
-  senha VARCHAR(30),
+  senha VARCHAR(40),
+  tipo VARCHAR(40),
   fk_faculdade INT NOT NULL,
   tipo_usuario VARCHAR(10),
   FOREIGN KEY (fk_faculdade)
@@ -23,7 +24,7 @@ CREATE TABLE faculdade(
 
 CREATE TABLE andar (
   id_andar INT PRIMARY KEY AUTO_INCREMENT,
-  identificador_andar VARCHAR(255),
+  identificador_andar VARCHAR(40),
   descricao_andar VARCHAR(255),
   fk_faculdade INT,
   FOREIGN KEY (fk_faculdade)
@@ -32,8 +33,8 @@ CREATE TABLE andar (
 
 CREATE TABLE sala (
   id_sala INT PRIMARY KEY AUTO_INCREMENT,
-  identificador_sala VARCHAR(255),
-  descricao_sala VARCHAR(10),
+  identificador_sala VARCHAR(40),
+  descricao_sala VARCHAR(255),
   fk_andar INT,
   FOREIGN KEY (fk_andar)
   REFERENCES andar (id_andar)
@@ -41,11 +42,13 @@ CREATE TABLE sala (
   
 CREATE TABLE computador (
   id_computador INT PRIMARY KEY AUTO_INCREMENT,
-  processador VARCHAR(40),
-  placa_mae VARCHAR(40) ,
+  hostname VARCHAR(100),
+  processador VARCHAR(100),
+  arquitetura VARCHAR (10),
+  fabricante VARCHAR(100),
   ram INT,
-  memoria INT,
-  sistema_operacional VARCHAR(20),
+  disco INT,
+  sistema_operacional VARCHAR(40),
   ativo BOOLEAN,
   problema_fisico BOOLEAN,
   problema_memoria BOOLEAN,
@@ -72,9 +75,11 @@ CREATE TABLE relatorio (
 );
   
 CREATE TABLE processo (
-	id_relatorio INT PRIMARY KEY AUTO_INCREMENT,
-    descricao VARCHAR(100),
-    data_hora DATETIME,
+	id_processo INT PRIMARY KEY AUTO_INCREMENT,
+    pid INT,
+    nome VARCHAR(100),
+    uso_cpu INT,
+    uso_memoria INT,
     fk_computador INT,
     FOREIGN KEY (fk_computador)
     REFERENCES computador (id_computador)
