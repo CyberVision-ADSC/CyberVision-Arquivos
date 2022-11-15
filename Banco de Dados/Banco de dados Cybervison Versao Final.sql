531a3537811ce4c7878ef31ed4154a74f55ed970
Leonardo Nacagawa Silva <leonardo.nsilva@sptech.school>
2022-11-15 14:06:59 -0300

    Versão final do banco atualizada

--- Banco de Dados/Banco de dados Cybervison Versao Final.sql
+++ Banco de Dados/Banco de dados Cybervison Versao Final.sql
@@ -1,3 +1,4 @@
+DROP DATABASE cybervision;
 CREATE DATABASE cybervision;
 USE cybervision;
 
@@ -9,7 +10,7 @@ CREATE TABLE faculdade(
   cep CHAR(8),
   numero INT
 );
-  
+
   CREATE TABLE usuario (
   id_usuario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(40),
@@ -17,6 +18,7 @@ CREATE TABLE faculdade(
   senha VARCHAR(40),
   tipo_usuario VARCHAR(40),
   fk_faculdade INT NOT NULL,
+  is_ativo BOOLEAN,
   FOREIGN KEY (fk_faculdade)
   REFERENCES faculdade (id_faculdade)
 );
@@ -26,6 +28,7 @@ CREATE TABLE andar (
   identificador_andar VARCHAR(40),
   descricao_andar VARCHAR(255),
   fk_faculdade INT,
+  is_ativo BOOLEAN,
   FOREIGN KEY (fk_faculdade)
   REFERENCES faculdade (id_faculdade)
 );
@@ -35,33 +38,36 @@ CREATE TABLE sala (
   identificador_sala VARCHAR(40),
   descricao_sala VARCHAR(255),
   fk_andar INT,
+  is_ativo BOOLEAN,
   FOREIGN KEY (fk_andar)
   REFERENCES andar (id_andar)
 );
   
 CREATE TABLE computador (
   id_computador INT PRIMARY KEY AUTO_INCREMENT,
+  identificador_computador VARCHAR(100),
   hostname VARCHAR(100),
   processador VARCHAR(100),
   arquitetura INT,
   fabricante VARCHAR(100),
   ram LONG,
   disco LONG,
+  problema_cpu BOOLEAN,
+  problema_disco BOOLEAN,
+  problema_memoria BOOLEAN,
+  problema_fisico BOOLEAN,
   sistema_operacional VARCHAR(100),
-  ativo BOOLEAN,
+  is_ativo BOOLEAN,
   fk_sala INT,
   FOREIGN KEY (fk_sala)
   REFERENCES sala (id_sala)
 );
+
 CREATE TABLE relatorio (
   id_relatorio INT PRIMARY KEY AUTO_INCREMENT,
   uso_cpu DOUBLE,
   uso_disco LONG,
   uso_ram LONG,
-  problema_cpu BOOLEAN,
-  problema_disco BOOLEAN,
-  problema_memoria BOOLEAN,
-  problema_fisico BOOLEAN,
   data_hora DATETIME,
   fk_computador INT,
   FOREIGN KEY (fk_computador)
@@ -82,30 +88,28 @@ CREATE TABLE processo (
     REFERENCES computador (id_computador)
 );
 
-
 insert into faculdade values(
 null, 'Sptech', 'sptech', '111111111111111', '88888888', 3388);
 
 insert into usuario values(
-null, 'Leonardo', 'leo@gmail.com', '123456', 'admin', 1);
+null, 'Leonardo', 'leo@gmail.com', '123456', 'admin', 1, true);
 
 insert into andar values(
-null, 'Andar 1', 'Andar legal', 1
+null, 'Andar 1', 'Andar legal', true, 1
 );
 
 insert into sala values (
-null, 'Sala 2ADSC', 'Sala legal', 1
+null, 'Sala 2ADSC', 'Sala legal', true, 1
 );
 
-insert into computador (id_computador, hostname, fk_sala) values(
-null, '123456789', 1
+insert into computador (id_computador, hostname, is_ativo, fk_sala) values(
+null, '123456789', true, 1
 );
+
 select * from faculdade;
+select * from andar;
+select * from sala;
 select * from usuario;
 select * from computador;
 select * from relatorio;
 select * from processo;
-
-insert into computador (id_computador, hostname, fk_sala) values(
-null, '123', 1
-);
