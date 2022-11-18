801c930d22a308427c6901504025b18742f4a8d2
Leonardo Nacagawa Silva <leonardo.nsilva@sptech.school>
2022-11-18 19:26:27 -0300

    Atualizacao no banco e diagrama de classes

--- Banco de Dados/CyberVision VFINAL.sql
+++ Banco de Dados/CyberVision VFINAL.sql
@@ -1,4 +1,3 @@
-DROP DATABASE cybervision;
 CREATE DATABASE cybervision;
 USE cybervision;
 
@@ -10,7 +9,7 @@ CREATE TABLE faculdade(
   cep CHAR(8),
   numero INT
 );
-
+  
   CREATE TABLE usuario (
   id_usuario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(40),
@@ -18,7 +17,6 @@ CREATE TABLE faculdade(
   senha VARCHAR(40),
   tipo_usuario VARCHAR(40),
   fk_faculdade INT NOT NULL,
-  is_ativo BOOLEAN,
   FOREIGN KEY (fk_faculdade)
   REFERENCES faculdade (id_faculdade)
 );
@@ -28,7 +26,6 @@ CREATE TABLE andar (
   identificador_andar VARCHAR(40),
   descricao_andar VARCHAR(255),
   fk_faculdade INT,
-  is_ativo BOOLEAN,
   FOREIGN KEY (fk_faculdade)
   REFERENCES faculdade (id_faculdade)
 );
@@ -38,36 +35,33 @@ CREATE TABLE sala (
   identificador_sala VARCHAR(40),
   descricao_sala VARCHAR(255),
   fk_andar INT,
-  is_ativo BOOLEAN,
   FOREIGN KEY (fk_andar)
   REFERENCES andar (id_andar)
 );
- 
+  
 CREATE TABLE computador (
   id_computador INT PRIMARY KEY AUTO_INCREMENT,
-  identificador_computador VARCHAR(100),
-  hostname VARCHAR(100) UNIQUE,
+  hostname VARCHAR(100),
   processador VARCHAR(100),
   arquitetura INT,
   fabricante VARCHAR(100),
   ram LONG,
   disco LONG,
-  problema_cpu BOOLEAN,
-  problema_disco BOOLEAN,
-  problema_memoria BOOLEAN,
-  problema_fisico BOOLEAN,
   sistema_operacional VARCHAR(100),
-  is_ativo BOOLEAN,
+  ativo BOOLEAN,
   fk_sala INT,
   FOREIGN KEY (fk_sala)
   REFERENCES sala (id_sala)
 );
-
 CREATE TABLE relatorio (
   id_relatorio INT PRIMARY KEY AUTO_INCREMENT,
   uso_cpu DOUBLE,
   uso_disco LONG,
   uso_ram LONG,
+  problema_cpu BOOLEAN,
+  problema_disco BOOLEAN,
+  problema_memoria BOOLEAN,
+  problema_fisico BOOLEAN,
   data_hora DATETIME,
   fk_computador INT,
   FOREIGN KEY (fk_computador)
@@ -78,31 +72,14 @@ CREATE TABLE relatorio (
 );
 
 CREATE TABLE processo (
-id_processo INT PRIMARY KEY AUTO_INCREMENT,
-pid INT,
-nome VARCHAR(100),
-uso_cpu DOUBLE,
-uso_memoria DOUBLE,
-data_hora_atualizado DATETIME,
-fk_computador INT,
-FOREIGN KEY (fk_computador)
-REFERENCES computador (id_computador)
-);
-
-CREATE TABLE chamados (
-id_chamado INT PRIMARY KEY AUTO_INCREMENT,
-ra_aluno VARCHAR(50),
-hostname VARCHAR(100),
-descricao_ocorrido VARCHAR(500),
-status_chamado CHAR(10), CHECK (status_chamado = 'Pendente' or status_chamado = 'Finalizado'),
-data_hora_criacao DATETIME,
-data_hora_conclusao DATETIME,
-fk_computador INT,
-FOREIGN KEY (fk_computador)
-REFERENCES computador (id_computador),
-fk_responsavel INT,
-FOREIGN KEY (fk_responsavel)
-REFERENCES usuario (id_usuario)
+	id_processo INT PRIMARY KEY AUTO_INCREMENT,
+    pid INT, 
+	nome VARCHAR(100),
+    uso_cpu DOUBLE,
+	uso_memoria DOUBLE,
+    fk_computador INT,
+    FOREIGN KEY (fk_computador)
+    REFERENCES computador (id_computador)
 );
 
 
@@ -110,25 +87,25 @@ insert into faculdade values(
 null, 'Sptech', 'sptech', '111111111111111', '88888888', 3388);
 
 insert into usuario values(
-null, 'Leonardo', 'leo@gmail.com', '123456', 'admin', 1, true);
+null, 'Leonardo', 'leo@gmail.com', '123456', 'admin', 1);
 
 insert into andar values(
-null, 'Andar 1', 'Andar legal', true, 1
+null, 'Andar 1', 'Andar legal', 1
 );
 
 insert into sala values (
-null, 'Sala 2ADSC', 'Sala legal', true, 1
+null, 'Sala 2ADSC', 'Sala legal', 1
 );
 
-insert into computador (id_computador, hostname, is_ativo, fk_sala) values(
-null, '123456789', true, 1
+insert into computador (id_computador, hostname, fk_sala) values(
+null, '123456789', 1
 );
-
 select * from faculdade;
-select * from andar;
-select * from sala;
 select * from usuario;
 select * from computador;
 select * from relatorio;
 select * from processo;
-select * from chamados;
+
+insert into computador (id_computador, hostname, fk_sala) values(
+null, '123', 1
+);
